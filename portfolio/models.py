d9044d101b2a35536b9eb0e8bd8091657de4ab31
davy1ex <dezy1ex@gmail.com>
2020-03-01 20:36:44 +0400

    added adding projects

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=64)
+    status = models.CharField(max_length=32)
+    desctiption = models.CharField(max_length=100)
+    photo = models.CharField(max_length=256)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+from .models import Project
+
+
 def index(request):
-    return render(request, "portfolio/index.html")
+    projects = Project.objects.all()
+    return render(request, "portfolio/index.html", {"projects":projects})
+
