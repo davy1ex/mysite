3816dbe4651383a4ac99c5411af6f782f957adbb
davy1ex <dezy1ex@gmail.com>
2020-03-02 20:42:01 +0400

    redesign

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 import portfolio.views 
 
 
@@ -8,3 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', portfolio.views.index)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/models.py
+++ portfolio/models.py
@@ -5,5 +5,5 @@ from django import forms
 class Project(models.Model):
     title = models.CharField(max_length=64)
     status = models.CharField(max_length=32)
-    desctiption = models.CharField(max_length=100)
-    photo = models.CharField(max_length=256)
+    description = models.CharField(max_length=100)
+    photo = models.ImageField(max_length=256)
